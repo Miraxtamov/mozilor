137485ddf4317ad498a3613d66a09ada38cd32c0
Miraxtamov <jmiraxtamov@gmail.com>
2022-09-01 18:31:29 +0500

    Add Spinner

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,27 +1,45 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Navbar from "../components/Navbar";
 import Generic from "../pages/Generic";
 import { navbar } from "../utils/navbar";
+import DotLoader from "react-spinners/DotLoader";
 
 const Root = () => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 1500);
+	}, []);
+
 	return (
-		<Routes>
-			<Route element={<Navbar />}>
-				<Route path="/" element={<Home />} />
-				{navbar?.map((value) => {
-					return (
-						<Route
-							key={value?.id}
-							path={value?.path}
-							element={value?.element}
-						/>
-					);
-				})}
-				<Route path="/career" element={<Generic />} />
-			</Route>
-		</Routes>
+		<>
+			{loading ? (
+				<div className="loader">
+					<DotLoader size={60} color={"#bf0830"} loading={loading} />
+				</div>
+			) : (
+				<Routes>
+					<Route element={<Navbar />}>
+						<Route path="/" element={<Home />} />
+						{navbar?.map((value) => {
+							return (
+								<Route
+									key={value?.id}
+									path={value?.path}
+									element={value?.element}
+								/>
+							);
+						})}
+						<Route path="/career" element={<Generic />} />
+					</Route>
+				</Routes>
+			)}
+		</>
 	);
 };
 
